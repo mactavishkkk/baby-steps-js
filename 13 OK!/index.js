014249e58bfd37ebc35fb38a39569415308d7764
Isaias Leite <isaiasleite230@gmail.com>
2022-05-23 13:04:52 -0300

    Upgrade in calculator

--- 13 OK!/index.js
+++ 13 OK!/index.js
@@ -1,15 +1,22 @@
 function calc(){
     let num1 = document.getElementById("n1").value;
     let num2 = document.getElementById("n2").value;
-    let sum = Number(num1) + Number(num2);
-    let sub = Number(num1) - Number(num2);
-    let mult = Number(num1) * Number(num2);
-    let div = Number(num1) / Number(num2);
-    let exp = Number(num1) ** Number(num2);
+    let op = document.getElementById("op").value;
 
-    document.getElementById("saida").innerHTML = `The result of ${num1} + ${num2} é = ${sum}.`;
-    document.getElementById("saida1").innerHTML = `The result of ${num1} - ${num2} é = ${sub}.`;
-    document.getElementById("saida2").innerHTML = `The result of ${num1} * ${num2} é = ${mult}.`;
-    document.getElementById("saida3").innerHTML = `The result of ${num1} / ${num2} é = ${div}.`;
-    document.getElementById("saida4").innerHTML = `The result of ${num1}^${num2} é = ${exp}.`;
+    switch(op){
+        case "+":
+            result = (Number(num1) + Number(num2))
+        break;
+        case "-":
+            result = num1 - num2
+        break;
+        case "*":
+            result = num1 * num2
+        break;
+        case "/":
+            result = num1 / num2
+        break;
+    };
+    
+    document.getElementById("saida").innerHTML = `The result is ${result}`;
 }
