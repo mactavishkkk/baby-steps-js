3516ab378d722438b0de2fab4edf7fcdee10ff8e
Isaías Leite <90873499+mactavishkkk@users.noreply.github.com>
2022-05-26 16:26:43 -0300

    Update really final

--- 19/index6.js
+++ 19/index6.js
@@ -10,13 +10,7 @@ function dispArr(){
 function dispArr1(){
     let x = (numeros.length)-1;
     for(let i = 0; i <= x; i++){
-        if((numeros[i] % 2) == 0){
-            triplo.push(numeros[i] * 3);
-        }else if((numeros[i] % 2) == 1){
-            triplo.push(numeros[i] * 3);
+        triplo.push(numeros[i] * 3);
     }
-}
-
-    document.getElementById("saida").innerHTML = numeros;
     document.getElementById("saida1").innerHTML = triplo;
 }
